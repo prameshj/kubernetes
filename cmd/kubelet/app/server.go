0b816031d2a18bbce29c8da25e84eb63e00d4aeb
Derek Carr <decarr@redhat.com>
2018-03-16 14:45:14 -0400

    Fix cpu cfs quota flag with pod cgroups

--- cmd/kubelet/app/server.go
+++ cmd/kubelet/app/server.go
@@ -662,6 +662,7 @@ func run(s *options.KubeletServer, kubeDeps *kubelet.Dependencies) (err error) {
 				ExperimentalCPUManagerPolicy:          s.CPUManagerPolicy,
 				ExperimentalCPUManagerReconcilePeriod: s.CPUManagerReconcilePeriod.Duration,
 				ExperimentalPodPidsLimit:              s.PodPidsLimit,
+				EnforceCPULimits:                      s.CPUCFSQuota,
 			},
 			s.FailSwapOn,
 			devicePluginEnabled,
